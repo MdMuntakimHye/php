28e029d86c441a2439665c9f7d06ff9b282b0446
MdMuntakimHye <46520159+MdMuntakimHye@users.noreply.github.com>
2023-03-07 07:17:46 +0600

    Update form.php

--- form.php
+++ form.php
@@ -1,89 +1,130 @@
+<?php
+// Start the session
+session_start();
+
+// Initialize variables for form input
+$firstName = "";
+$lastName = "";
+$email = "";
+$address = "";
+$image = "";
+
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the form data
+    $firstName = test_input($_POST["firstName"]);
+    $lastName = test_input($_POST["lastName"]);
+    $email = test_input($_POST["email"]);
+    $address = test_input($_POST["address"]);
+
+    // Validate the image file
+    if ($_FILES["image"]["size"] > 0) {
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if($check === false) {
+            echo "Error: File is not an image.";
+        } elseif ($_FILES["image"]["size"] > 500000) {
+            echo "Error: File is too large.";
+        } elseif ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png" && $imageFileType != "gif") {
+            echo "Error: Only JPG, JPEG, PNG & GIF files are allowed.";
+        } else {
+            // Move the uploaded file to the uploads directory
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                $image = $target_file;
+            } else {
+                echo "Error: There was an error uploading your file.";
+            }
+        }
+    }
+
+    // Store the form data in SESSION variables
+    $_SESSION["firstName"] = $firstName;
+    $_SESSION["lastName"] = $lastName;
+    $_SESSION["email"] = $email;
+    $_SESSION["address"] = $address;
+    $_SESSION["image"] = $image;
+
+    // Redirect to the display page
+    header("Location: display.php");
+    exit();
+}
+
+// Function to validate form input
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        background-color: #f5f5f5;
-    }
+    <title>PHP Form Example</title>
+	<style>
+		p{
+			color: white;
+		}
 
-    .container {
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 20px;
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        border-radius: 10px;
-    }
-
-    h1 {
-        font-size: 32px;
-        margin-top: 0;
-    }
-
-    img {
-        max-width: 100%;
-        margin-bottom: 20px;
-    }
-
-    dl {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-gap: 20px;
-        margin: 0;
-    }
-
-    dt {
-        font-weight: bold;
-    }
-
-    @media (max-width: 768px) {
-        .container {
-            padding: 10px;
-            border-radius: 0;
+		.container {
+			
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            max-width: 800px;
+            background-color: #243447;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+			text
         }
-
-        h1 {
-            font-size: 24px;
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
         }
-
-        dl {
-            grid-template-columns: 1fr;
+        input[type="text"], input[type="email"], textarea, input[type="file"] {
+            margin: 10px 0;
+			color: white;
+            padding: 5px;
+            width: 300px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
         }
-    }
-  </style>
+        input[type="reset"], input[type="submit"] {
+            margin: 10px 0;
+            padding: 5px 10px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+        input[type="reset"]:hover, input[type="submit"]:hover {
+            background-color: #2E8B57;
+        }
+    </style>
 </head>
 <body>
-    
-    <div class="container">
-    
-    <form action="nextpage.php" method="POST" enctype="multipart/form-data">
-        First Name:
-        <input type="text" name="fname"><br><br>
-        Last Name:
-        <input type="text" name="lname"><br><br>
-        Email:
-        <input type="email" name="email"><br><br>
-        Address:
-        <input type="text" name="adress"><br><br>
-        Upload Image:
-        <input type="file" name="upimage"><br><br>
-        
-        <input type="reset">
-        <input type="submit"/>
+	<div class="container">
 
-        
-
-        </div>
-
-
-    
-      
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
+        <label for="firstName"> <p>First Name:</p></label>
+        <input type="text" id="firstName" name="firstName" value="<?php echo $firstName; ?>" required>
+        <label for="lastName"><p>Last Name:</p> </label>
+        <input type="text" id="lastName" name="lastName" value="<?php echo $lastName; ?>" required>
+        <label for="email"><p>Email:</p> Email:</label>
+        <input type="email" id="email" name="email" value="<?php echo $email; ?>" required>
+        <label for="address"><p>Address: </p></label>
+        <textarea id="address" name="address"><?php echo $address; ?></textarea>
+        <label for="image"><p>Image:</p> </label>
+        <input type="file" id="image" name="image">
+        <input type="reset" value="Cancel">
+        <input type="submit" value="Submit">
 </body>
+	</div>
+
 </html>
